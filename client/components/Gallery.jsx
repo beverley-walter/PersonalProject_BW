d1f4b7f3bae3f74ba6fdeb690604e487a8f52951
Bev Walter <bevwaltersemail@gmail.com>
2017-05-30 18:35:15 +1200

    new bundle file- again

--- client/components/Gallery.jsx
+++ client/components/Gallery.jsx
@@ -7,21 +7,21 @@ const Gallery = props => {
             <div className="gallery_item four columns">
                <Link to="/">
                <span className= "button" for="Baby Grow 1"> Awesome!<br/> For Age: 3-6 months </span>
-               <img src="../images/babyg_1.jpg" alt="Clothing Photo" />
+               <img src="images/babyg_1.jpg" alt="Clothing Photo" />
                </Link>
             </div>
 
             <div className="gallery_item four columns">
                <Link to="/">
                <span className= "button" for="Baby Grow 2"> Cute As!<br/> For Age: 3-6 months </span>
-               <img src="../images/babyg_2.jpg" alt="Clothing Photo" />
+               <img src="images/babyg_2.jpg" alt="Clothing Photo" />
                </Link>
             </div>
 
             <div className="gallery_item four columns">
                <Link to="/">
                <span className= "button" for="Baby Grow 3"> Perfect!<br/> For Age: 0-3 months </span>
-               <img src="../images/babyg_3.jpg" alt="Clothing Photo" />
+               <img src="images/babyg_3.jpg" alt="Clothing Photo" />
                </Link>
             </div>
       </div>
